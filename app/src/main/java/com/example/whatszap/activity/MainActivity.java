924e3f5220993fae1c73bc8eb946de2d0256a58c
Gustavo Souza <gtssouza94@gmail.com>
2020-08-02 01:19:19 -0300

    Adicionando menus no MainActivity

--- app/src/main/java/com/example/whatszap/activity/MainActivity.java
+++ app/src/main/java/com/example/whatszap/activity/MainActivity.java
@@ -1,21 +1,61 @@
 package com.example.whatszap.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.whatszap.R;
+import com.example.whatszap.config.ConfigFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        autenticacao = ConfigFirebase.getFirebaseAutentication();
+
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("WhatZap");
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair :
+                deslogarUsuario();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
