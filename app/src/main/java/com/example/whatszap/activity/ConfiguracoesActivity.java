36571a88ede0e05767c83e0b4cc277908654fe48
Gustavo Souza <gtssouza94@gmail.com>
2020-08-02 11:34:09 -0300

    ConfiguracoesActivity criada e atalho para MainActivity criado

---
+++ app/src/main/java/com/example/whatszap/activity/ConfiguracoesActivity.java
@@ -0,0 +1,23 @@
+package com.example.whatszap.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.whatszap.R;
+
+public class ConfiguracoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Configurações");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
