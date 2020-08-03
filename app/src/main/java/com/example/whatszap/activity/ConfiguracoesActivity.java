d4b2116810bb5ba6776dd61c8dfff085c522d47c
Gustavo Souza <gtssouza94@gmail.com>
2020-08-03 10:59:17 -0300

    Configurando Camera para escolha de fotos

--- app/src/main/java/com/example/whatszap/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/whatszap/activity/ConfiguracoesActivity.java
@@ -7,13 +7,21 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.whatszap.R;
 import com.example.whatszap.helper.Permissao;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
+
+    private ImageButton btnCamera, btnGaleria;
+    private static final int SELECAO_CAMERA=100;
+    private static final int SELECAO_GALERIA=200;
 
     private String[] permissioesNecessarias = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -25,6 +33,9 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
 
+        btnCamera = findViewById(R.id.imageBtnCamera);
+        btnGaleria = findViewById(R.id.imageBtnGaleria);
+
         //Valida permissões
         Permissao.validarPermissoes(permissioesNecessarias,this,1);
 
@@ -33,6 +44,18 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(i.resolveActivity(getPackageManager()) != null){
+                    //Começa a activity e captura o resultado
+                    startActivityForResult(i,SELECAO_CAMERA);
+                }
+
+            }
+        });
     }
 
     @Override
