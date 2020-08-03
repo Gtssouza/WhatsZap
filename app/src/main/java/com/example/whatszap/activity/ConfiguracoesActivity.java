40d3152f18bb93cda9dbb2999083049efa19e8d4
Gustavo Souza <gtssouza94@gmail.com>
2020-08-02 21:40:59 -0300

    Permissões para camera e galeria de fotos implementadas

--- app/src/main/java/com/example/whatszap/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/whatszap/activity/ConfiguracoesActivity.java
@@ -1,9 +1,13 @@
 package com.example.whatszap.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.whatszap.R;
@@ -22,7 +26,7 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_configuracoes);
 
         //Valida permissões
-        Permissao.validarPermissoes(permissioesNecessarias,this);
+        Permissao.validarPermissoes(permissioesNecessarias,this,1);
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configurações");
@@ -30,4 +34,32 @@ public class ConfiguracoesActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int permissaoResult: grantResults){
+            if(permissaoResult == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para ultilizar o app é necessario aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
--- app/src/main/java/com/example/whatszap/helper/Permissao.java
+++ app/src/main/java/com/example/whatszap/helper/Permissao.java
@@ -1,9 +1,11 @@
 package com.example.whatszap.helper;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
@@ -11,7 +13,7 @@ import java.util.List;
 
 public class Permissao {
 
-    public static boolean validarPermissoes(String[] permissoes, Activity activity){
+    public static boolean validarPermissoes(String[] permissoes, Activity activity, int requestCode){
 
         if(Build.VERSION.SDK_INT>=23){
             List<String> listaPermissoes = new ArrayList<>();
@@ -19,8 +21,19 @@ public class Permissao {
             /*Percorre as permissões passadas verificando
             uma a uma se já tem a permissão liberda*/
             for(String permissao : permissoes){
-                ContextCompat.checkSelfPermission(activity,permissao);
+                Boolean temPermissao = ContextCompat.checkSelfPermission(activity,permissao) == PackageManager.PERMISSION_GRANTED;
+                if(!temPermissao) listaPermissoes.add(permissao);
             }
+            /*Caso a lista esteja vazia, não é necessario solicitar permissão*/
+            if(listaPermissoes.isEmpty()){
+                return true;
+            }
+
+            String[] novasPermissoes = new String[listaPermissoes.size()];
+            listaPermissoes.toArray(novasPermissoes);
+
+            /*Solicita permissão*/
+            ActivityCompat.requestPermissions(activity,novasPermissoes,requestCode);
 
         }
 
