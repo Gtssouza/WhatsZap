75d245deb5685ef7d2ebb48da8e2af3520efec08
Gustavo Souza <gtssouza94@gmail.com>
2020-08-09 18:05:31 -0300

    Layout ChatActivity finalizado

--- app/src/main/java/com/example/whatszap/activity/ChatActivity.java
+++ app/src/main/java/com/example/whatszap/activity/ChatActivity.java
@@ -16,8 +16,17 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
