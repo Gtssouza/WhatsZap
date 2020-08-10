28ad5422c087345e5fa9852c64595787193ecba0
Gustavo Souza <gtssouza94@gmail.com>
2020-08-10 09:47:51 -0300

    Salvando mensagem no Firebase

--- app/src/main/java/com/example/whatszap/activity/ChatActivity.java
+++ app/src/main/java/com/example/whatszap/activity/ChatActivity.java
@@ -6,15 +6,22 @@ import androidx.appcompat.widget.Toolbar;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.whatszap.R;
+import com.example.whatszap.config.ConfigFirebase;
+import com.example.whatszap.helper.Base64Custom;
 import com.example.whatszap.helper.UsuarioFirebase;
+import com.example.whatszap.model.Mensagem;
 import com.example.whatszap.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -23,6 +30,11 @@ public class ChatActivity extends AppCompatActivity {
     private CircleImageView imgPerfilChat;
     private TextView txtNomeChat;
     private Usuario userDest;
+    private EditText txtMsg;
+
+    //Identificadores de Usuario remetente e destinatario
+    private String idUserRemetente;
+    private String idUserDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +45,12 @@ public class ChatActivity extends AppCompatActivity {
 
         imgPerfilChat = findViewById(R.id.circleImagePerfil);
         txtNomeChat = findViewById(R.id.txtChatNome);
+        txtMsg = findViewById(R.id.editEnviarText);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //Recupera dados do user remetente
+        idUserRemetente = UsuarioFirebase.getIndentificadorUser();
+
+        FloatingActionButton fab = findViewById(R.id.fabEnviar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
@@ -59,7 +68,38 @@ public class ChatActivity extends AppCompatActivity {
                 imgPerfilChat.setImageResource(R.drawable.padrao);
             }
 
+            //recuperar dados usuario destinatario
+            idUserDestinatario = Base64Custom.codificaBase64(userDest.getEmail());
+
         }
 
     }
+
+    public void enviarMsg(View view){
+        String msg = txtMsg.getText().toString();
+        if(!msg.isEmpty()){
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUser(idUserRemetente);
+            mensagem.setMensagem(msg);
+
+            //salvar mensagem
+            salvarMensagem(idUserRemetente,idUserDestinatario,mensagem);
+
+        }else{
+            Toast.makeText(ChatActivity.this,"Escreva uma mensagem",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void salvarMensagem(String remetente, String destinatario, Mensagem txtMensg){
+        DatabaseReference dbRef = ConfigFirebase.getFirebaseDatabase();
+        DatabaseReference msgRef = dbRef.child("mensagens");
+
+        msgRef.child(remetente).child(destinatario).push()
+                .setValue(txtMensg);
+
+        //Limpar texto
+        txtMsg.setText("");
+    }
+
 }
---
+++ app/src/main/java/com/example/whatszap/model/Mensagem.java
@@ -0,0 +1,35 @@
+package com.example.whatszap.model;
+
+public class Mensagem {
+
+    private String idUser;
+    private String mensagem;
+    private String imagem;
+
+    public Mensagem() {
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}
