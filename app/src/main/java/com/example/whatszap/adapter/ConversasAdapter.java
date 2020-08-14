3d417dc66f116b54cf3690e99aa268635edb1e08
Gustavo Souza <gtssouza94@gmail.com>
2020-08-14 11:58:17 -0300

    Listando conversas, apenas imagens

--- app/src/main/java/com/example/whatszap/adapter/ConversasAdapter.java
+++ app/src/main/java/com/example/whatszap/adapter/ConversasAdapter.java
@@ -1,17 +1,23 @@
 package com.example.whatszap.adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.whatszap.R;
 import com.example.whatszap.model.Conversa;
+import com.example.whatszap.model.Usuario;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ConversasAdapter extends RecyclerView.Adapter<ConversasAdapter.MyViewHolder> {
 
@@ -32,17 +38,36 @@ public class ConversasAdapter extends RecyclerView.Adapter<ConversasAdapter.MyVi
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Conversa conversa = conversas.get(position);
+        holder.ultimaMSg.setText(conversa.getUltimaMsg());
 
+        Usuario usuario = conversa.getUsuarioExibicao();
+        holder.nome.setText(usuario.getNome());
+
+        if(usuario.getFoto() != null){
+            Uri uri = Uri.parse(usuario.getFoto());
+            Glide.with(c).load(uri).into(holder.foto);
+        }else{
+            holder.foto.setImageResource(R.drawable.padrao);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return conversas.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView foto;
+        TextView nome, ultimaMSg;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            foto = itemView.findViewById(R.id.imgPerfilAdapter);
+            nome = itemView.findViewById(R.id.txtNomeAdapter);
+            ultimaMSg = itemView.findViewById(R.id.txtEmailAdapter);
         }
     }
 }
--- app/src/main/java/com/example/whatszap/fragment/ConversasFragment.java
+++ app/src/main/java/com/example/whatszap/fragment/ConversasFragment.java
@@ -2,6 +2,8 @@ package com.example.whatszap.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +18,10 @@ import com.example.whatszap.adapter.ConversasAdapter;
 import com.example.whatszap.config.ConfigFirebase;
 import com.example.whatszap.helper.UsuarioFirebase;
 import com.example.whatszap.model.Conversa;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +31,9 @@ public class ConversasFragment extends Fragment {
     private RecyclerView recyclerViewConversas;
     private ConversasAdapter adapterConversas;
     private List<Conversa> listaConversas = new ArrayList<>();
+    private DatabaseReference dbRef;
+    private DatabaseReference conversaRef;
+    private ChildEventListener childEventListenerConversas;
 
     public ConversasFragment() {
 
@@ -36,8 +45,8 @@ public class ConversasFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_conversas, container, false);
         recyclerViewConversas = view.findViewById(R.id.recyclerIdConversas);
-        userRef = ConfigFirebase.getFirebaseDatabase().child("usuarios");
-        userAtual = UsuarioFirebase.getUserAtual();
+       // userRef = ConfigFirebase.getFirebaseDatabase().child("usuarios");
+        //userAtual = UsuarioFirebase.getUserAtual();
 
         //Configurar adapter
         adapterConversas = new ConversasAdapter(listaConversas, getActivity());
@@ -46,6 +55,58 @@ public class ConversasFragment extends Fragment {
         recyclerViewConversas.setLayoutManager(layoutManager);
         recyclerViewConversas.setHasFixedSize(true);
         recyclerViewConversas.setAdapter(adapterConversas);
+
+        //Configura conversas ref
+        String identificadorUser = UsuarioFirebase.getIndentificadorUser();
+        dbRef = ConfigFirebase.getFirebaseDatabase();
+        conversaRef = dbRef.child("conversas").child(identificadorUser);
+
+
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarConveras();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        conversaRef.removeEventListener(childEventListenerConversas);
+    }
+
+    public void recuperarConveras(){
+
+        childEventListenerConversas = conversaRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                //Recuperar conversas
+                Conversa conversa = dataSnapshot.getValue(Conversa.class);
+                listaConversas.add(conversa);
+                adapterConversas.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
