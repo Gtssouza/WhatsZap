1f2a1a15988b985b69192666240f3f50de9cc34f
Gustavo Souza <gtssouza94@gmail.com>
2020-08-17 16:11:57 -0300

    Criando interface de cadastro de grupo

--- app/src/main/java/com/example/whatszap/activity/CadastroGrupoActivity.java
+++ app/src/main/java/com/example/whatszap/activity/CadastroGrupoActivity.java
@@ -2,12 +2,15 @@ package com.example.whatszap.activity;
 
 import android.os.Bundle;
 
+import com.example.whatszap.adapter.GrupoSelecionadoAdapter;
 import com.example.whatszap.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.widget.TextView;
@@ -20,7 +23,9 @@ import java.util.List;
 public class CadastroGrupoActivity extends AppCompatActivity {
 
     private List<Usuario> usuariosListaSelect = new ArrayList<>();
-    private TextView txtTotal;
+    private TextView txtParticipantes;
+    private GrupoSelecionadoAdapter adapterGrupo;
+    private RecyclerView recyclerGrupoSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +33,11 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_grupo);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle("Novo Grupo");
+        toolbar.setSubtitle("Defina o nome");
 
-        txtTotal = findViewById(R.id.textViewtotal);
+        txtParticipantes = findViewById(R.id.textTotalParticipantes);
+        recyclerGrupoSelect = findViewById(R.id.recyclerCadastroGrupo);
 
         FloatingActionButton fab = findViewById(R.id.fabConfirmaGrupo);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +54,15 @@ public class CadastroGrupoActivity extends AppCompatActivity {
             List<Usuario> membros = (List<Usuario>) getIntent().getExtras().getSerializable("membros");
             usuariosListaSelect.addAll(membros);
 
-            txtTotal.setText("total :" + usuariosListaSelect.size());
+            txtParticipantes.setText("Membros: " + usuariosListaSelect.size());
         }
+
+        //Configurar recyclerView
+        adapterGrupo = new GrupoSelecionadoAdapter(usuariosListaSelect, getApplicationContext());
+        //configuração recyclerView
+        RecyclerView.LayoutManager layoutManagerHorizontal = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false);
+        recyclerGrupoSelect.setLayoutManager(layoutManagerHorizontal);
+        recyclerGrupoSelect.setHasFixedSize(true);
+        recyclerGrupoSelect.setAdapter(adapterGrupo);
     }
 }
