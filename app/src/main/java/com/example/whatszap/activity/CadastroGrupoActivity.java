dccabc5b83f2de4e3d96ffdd7d5014618e402968
Gustavo Souza <gtssouza94@gmail.com>
2020-08-18 14:36:26 -0300

    Voltando para ChatActivity depois da criação de novo grupo

--- app/src/main/java/com/example/whatszap/activity/CadastroGrupoActivity.java
+++ app/src/main/java/com/example/whatszap/activity/CadastroGrupoActivity.java
@@ -108,6 +108,10 @@ public class CadastroGrupoActivity extends AppCompatActivity {
 
                 grupo.setNome(grupoNome);
                 grupo.salvar();
+
+                Intent i = new Intent(CadastroGrupoActivity.this, ChatActivity.class);
+                i.putExtra("chatGrupo",grupo);
+                startActivity(i);
             }
         });
     }
